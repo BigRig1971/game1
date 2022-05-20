d6d22a252bcd0208bc18b18e69428edd1e8ba612
Tracy Huntsberger <tracyhuntsberger@gmail.com>
2022-05-20 13:08:47 -0500

    Revert "graphics"
    This reverts commit c05d2bd7646abda02f21df422860e105ecdb765a.

--- Assets/aiFolder/Models/Ai_Scripts/AiGeneric.cs
+++ Assets/aiFolder/Models/Ai_Scripts/AiGeneric.cs
@@ -85,7 +85,10 @@ namespace StupidHumanGames
 
         private void OnValidate()
         {
+
+
             meshReference = GetComponentInChildren<SkinnedMeshRenderer>();
+
             sizeOfObject = new Vector3(scale, scale, scale);
             transform.localScale = sizeOfObject;
         }
@@ -114,6 +117,16 @@ namespace StupidHumanGames
             if (distanceToHome >= maxRange || transform.position.y > maxAltitude || transform.position.y < minAltitude) return true; else return false;
         }
 
+
+        void NextState()
+        {
+            var l = System.Enum.GetNames(typeof(state)).Length;
+            _currentState++;
+            if ((int)_currentState >= l)
+            {
+                _currentState = 0;
+            }
+        }
         void SwimOrFly()
         {
             DistanceToWaypoint();
@@ -272,8 +285,10 @@ namespace StupidHumanGames
         {
             while (true)
             {
+
                 yield return StartCoroutine(_currentState.ToString());
             }
+
         }
         IEnumerator Patrol()
         {
@@ -282,6 +297,7 @@ namespace StupidHumanGames
                 moveSpeed = previousSpeed * .5f;
                 animator.SetFloat("Blend", .5f);
             }
+
             while (_currentState == state.Patrol && OnCanPatrol())
             {
                 RandomWaypoint();
@@ -305,6 +321,7 @@ namespace StupidHumanGames
         }
         IEnumerator Chase()
         {
+
             if (OnCanChase())
             {
                 moveSpeed = previousSpeed;
@@ -328,7 +345,11 @@ namespace StupidHumanGames
             }
             while (_currentState == state.Attack && OnCanAttack())
             {
+
+
                 RandomAttackAnimations();
+
+
                 wayPoint = player.position;
                 yield return null;
             }
